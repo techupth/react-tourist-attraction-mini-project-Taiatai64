5ef5d843853edf99da09b7cf9522790c55518514
Tai <5506726tai@gmail.com>
2024-01-04 14:34:11 +0700

    v1 but bug for no.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,51 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  let [searchText, setSearchText] = useState("กรุณาพิมพ์เพื่อค้นหา");
+
+  let [result, setResult] = useState([]);
+
+  const getTrip = (e) => {
+    setSearchText(e.target.value);
+    fetch("http://localhost:4001/trips?keywords=" + e.target.value)
+      .then((respon) => {
+        return respon.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setResult(data.data);
+      });
+  };
+  console.log(result);
+  return (
+    <div className="App">
+      {/* Start coding here */}
+
+      <h1 className="Head">เที่ยวไหนดี</h1>
+      <input onChange={getTrip} value={searchText}></input>
+      {result.map((trip) => {
+        return (
+          <div>
+            <div className="mainPhoto">
+              <img src={trip.photos[0]} />
+            </div>
+            <div>
+              <h1>{trip.title}</h1>
+              <p>{trip.description.slice(0, 100)}</p>{" "}
+              <a href={trip.url}>อ่านต่อ </a>
+              <p>{trip.tags}</p>
+              <div className="miniPhoto">
+                <img src={trip.photos[1]} />
+                <img src={trip.photos[2]} />
+                <img src={trip.photos[3]} />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
